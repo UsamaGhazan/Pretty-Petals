8d7de7222ed62ba41bfb013aee8134f41f9c1833
Usama <usamaghazan@yahoo.com>
2023-02-09 10:07:19 +0500

    Initial setup

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+
+function App() {
+  return <>hewe</>;
+}
+
+export default App;
---
+++ products.js
@@ -0,0 +1,29 @@
+const products = [
+  {
+    _id: '1',
+    name: 'SELENE',
+    image: '/images/p1.jpg',
+
+    desc: 'YELLOW GOLD HOOP EARRINGS',
+    price: 170,
+    countInStock: 10,
+  },
+  {
+    _id: '2',
+    name: 'RHEA',
+    image: '/images/p2.jpg',
+
+    desc: 'TWO TONE GOLD HOOP EARRINGS',
+    price: 210,
+    countInStock: 5,
+  },
+  {
+    _id: '3',
+    name: 'DIONE',
+    image: '/images/p3.jpg',
+
+    desc: 'GOLD DROP EARRINGS',
+    price: 200,
+    countInStock: 0,
+  },
+];
